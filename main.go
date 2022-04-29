5b2c9115f56bcf95415c72eb2a3ff63bb98e7652
MiyuYokoyama0109 <miyu.y@latona.io>
2022-04-29 14:50:39 +0900

    release

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	sap_api_caller "sap-api-integrations-maintenance-notification-reads/SAP_API_Caller"
-	"sap-api-integrations-maintenance-notification-reads/sap_api_input_reader"
+	"sap-api-integrations-maintenance-notification-reads/SAP_API_Input_Reader"
 
 	"github.com/latonaio/golang-logging-library-for-sap/logger"
 )
